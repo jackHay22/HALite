cceaa2ace3d168b46ec2599e3b04d1a40ccf919b
benprofitt <bparfitt@hamilton.edu>
2018-09-10 17:53:04 -0400

    working on basic structure for graphs

--- src/ui_graphlib/GraphPanel.java
+++ src/ui_graphlib/GraphPanel.java
@@ -1,22 +1,36 @@
 package ui_graphlib;
 
+import java.util.ArrayList;
+
 @SuppressWarnings("serial")
 public class GraphPanel extends ui_framework.SystemPanel {
-	//extends InteractivePanel 
+	//extends SystemPanel 
+	
+	private int width;
+	private int length;
+	private ArrayList<PointSet> point_sets;
+	
 	public GraphPanel() {
 		//super();
 	}
 	
-	private int width;
+	private void draw_graph() {
+		set_labels();
+		plot_points();
+	}
 	
-	private void PlotPoints() {
+	private void plot_points() {
+		
+	}
+	
+	private void set_labels() {
 		
 	}
 	
 	@Override
 	public void refresh() {
 		// TODO Auto-generated method stub
-
+		draw_graph();
 	}
 
 }
--- src/ui_graphlib/PointSet.java
+++ src/ui_graphlib/PointSet.java
@@ -5,8 +5,14 @@ import java.awt.Color;
 
 public class PointSet {
 	
+	private ArrayList<Point> points;
+	private double min_x;
+	private double max_x;
+	private double min_y;
+	private double max_y;
+	
 	public PointSet(ArrayList<Point> points, Color color, String x_axis, String y_axis) {
-		
+		this.points = points;
 	}
 	
 	
