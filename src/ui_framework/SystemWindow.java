41218a743ea33f8e8c730ff1e2918bc9fa53a86b
jackHay22 <jjphay@gmail.com>
2018-09-12 14:06:13 -0400

    formatting fix

--- src/ui_framework/SystemWindow.java
+++ src/ui_framework/SystemWindow.java
@@ -26,9 +26,9 @@ public class SystemWindow extends JFrame implements Refreshable{
 		
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(width, height);
-        this.validate();
-        this.setVisible(true);
+		this.setSize(width, height);
+    	this.validate();
+    	this.setVisible(true);
 	}
 	
 	public void set_minimum_size(int width, int height) {
