aeaefcd8ba7399f92308cb2df3f8416aee36efda
jackHay22 <jjphay@gmail.com>
2018-09-25 15:09:38 -0400

    fix settings panel

--- src/ui_stdlib/ImageButton.java
+++ src/ui_stdlib/ImageButton.java
@@ -1,5 +1,6 @@
 package ui_stdlib;
 
+import java.awt.Dimension;
 import java.awt.Image;
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
@@ -21,7 +22,8 @@ public class ImageButton extends JButton {
 	private void create_button(String resource_pos, int width, int height) {
 		try {
 			Image button_image = ImageIO.read(getClass().getResourceAsStream(resource_pos))
-										.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);			
+										.getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH);		
+			this.setPreferredSize(new Dimension(width, height));
 			this.setIcon(new ImageIcon(button_image));
 			Border emptyBorder = BorderFactory.createEmptyBorder();
 			this.setBorder(emptyBorder);
