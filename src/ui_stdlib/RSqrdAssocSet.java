a9aad578a554737f1990336b20a906745354a0c2
jackHay22 <jjphay@gmail.com>
2018-09-27 12:20:16 -0400

    fix update bug on rsqrd list element

--- src/ui_stdlib/RSqrdAssocSet.java
+++ src/ui_stdlib/RSqrdAssocSet.java
@@ -18,14 +18,16 @@ public class RSqrdAssocSet extends SystemPanel {
 	private boolean element_selected = false;
 	private DataStore datastore;
 	private Element this_element;
+	private Element this_element2;
 	
 	public RSqrdAssocSet(Element element1, Element element2, String val) {
 		super();
 		this.this_element = element1;
+		this.this_element2 = element2;
 		this.setLayout(new GridLayout(2,0));
 		Border border = BorderFactory.createLineBorder(SystemThemes.BACKGROUND);
 
-		element = new JButton(element2.toString());
+		element = new JButton(this.this_element2.toString());
 		value = new JButton(val);
 		
 		element.setBorder(BorderFactory.createCompoundBorder(border, 
@@ -40,11 +42,11 @@ public class RSqrdAssocSet extends SystemPanel {
 		    	if (!element_selected) {	
 					element.setOpaque(true);
 					element_selected = !element_selected;
-					datastore.set_selected_rsqrd_assocs(this_element, element2);
+					datastore.set_selected_rsqrd_assocs(this_element, this_element2);
 		    	} else {
 		    		element.setOpaque(false);
 		    		element_selected = !element_selected;
-		    		datastore.remove_selected_rsqrd_assocs(this_element, element2);
+		    		datastore.remove_selected_rsqrd_assocs(this_element, this_element2);
 		    	}
 		    }
 		});
@@ -59,6 +61,7 @@ public class RSqrdAssocSet extends SystemPanel {
 	
 	public void set(Element element1, Element element2, String val) {
 		this.this_element = element1;
+		this.this_element2 = element2;
 		element.setText(element2.toString());
 		value.setText(val);
 	}
