21815e8ae7418b0298b24c0f852f57e08e68e63c
benprofitt <bparfitt@hamilton.edu>
2018-10-18 18:47:10 -0400

    Added one commented out line. Waiting on datastore get method

--- src/system_utils/ElementCorrelationInfo.java
+++ src/system_utils/ElementCorrelationInfo.java
@@ -35,13 +35,12 @@ public class ElementCorrelationInfo implements Refreshable {
 		ArrayList<Point> point_list = new ArrayList<Point>();
 		
 		for (String std : data_store.get_STDlist()) {
-			Double x = WMs.get(std);
-			Double y = data_store.get_raw_std_elem(std, element);
+			Double x = data_store.get_raw_std_elem(std, element);
+			Double y = WMs.get(std);
+			//y = data_store.get_mean_value(std, element)/y;
 			if (x != null && y != null) {
 				point_list.add(new Point(x, y));
 				reg_obj.addData(x, y);
-				System.out.println(x);
-				System.out.println(y);
 			}
 		}
 		
