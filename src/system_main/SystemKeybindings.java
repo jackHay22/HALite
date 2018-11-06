0f30cdd93e2d8b607e0065efc97d3778c4c08818
jackHay22 <jjphay@gmail.com>
2018-11-06 14:26:53 -0500

    clean up mnemonics

--- src/system_main/SystemKeybindings.java
+++ src/system_main/SystemKeybindings.java
@@ -2,14 +2,14 @@ package system_main;
 
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-
 import javax.swing.KeyStroke;
 
 public class SystemKeybindings {
+	private static int command_shortcut =  Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
 	
 	//JMenu mnemonics 
-	public static final KeyStroke SAVE = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-	public static final KeyStroke EX_DATA = KeyStroke.getKeyStroke(KeyEvent.VK_E, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-	public static final KeyStroke NEW = KeyStroke.getKeyStroke(KeyEvent.VK_N, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
-	public static final KeyStroke CLOSE_WINDOW = KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask());
+	public static final KeyStroke SAVE = KeyStroke.getKeyStroke(KeyEvent.VK_S,command_shortcut);
+	public static final KeyStroke EX_DATA = KeyStroke.getKeyStroke(KeyEvent.VK_E,command_shortcut);
+	public static final KeyStroke NEW = KeyStroke.getKeyStroke(KeyEvent.VK_N,command_shortcut);
+	public static final KeyStroke CLOSE_WINDOW = KeyStroke.getKeyStroke(KeyEvent.VK_W,command_shortcut);
 }
