b68152574c5366f3a94f173e361bdcea3ccd634b
jackHay22 <jjphay@gmail.com>
2018-11-06 12:38:05 -0500

    updates to drift correction settings layout

--- src/system_drift_correction/DriftCorrectionSettings.java
+++ src/system_drift_correction/DriftCorrectionSettings.java
@@ -18,11 +18,20 @@ public class DriftCorrectionSettings<Backend extends DriftCorrectionDS> extends 
 	private JComboBox<Element> element_selection;
 	private JComboBox<Integer> degree_selection;
 	private int max_degree = 10;
+	private int default_degree = 6;
 	private int current_elem_index;
 	private int total_elems;
 	private JButton next_element;
 	private JButton prev_element;
 	private JButton calc_fit;
+	private int static_button_width;
+	
+	private PanelHeader<DriftCorrectionDS> panel_header;
+	private PanelHeader<DriftCorrectionDS> element_label;
+	private PanelHeader<DriftCorrectionDS> degree_label;
+	
+	private PanelHeader<DriftCorrectionDS> label_one; //TODO: rename
+	private PanelHeader<DriftCorrectionDS> label_two; //TODO: rename
 	
 	public DriftCorrectionSettings() {
 		super();
@@ -39,6 +48,15 @@ public class DriftCorrectionSettings<Backend extends DriftCorrectionDS> extends 
 		prev_element = new JButton("Previous Element");
 		calc_fit = new JButton("Calculate Fit");
 		prev_element.setEnabled(false);
+		
+		panel_header = new PanelHeader<DriftCorrectionDS>("Drift Correction", SystemThemes.MAIN);
+		element_label = new PanelHeader<DriftCorrectionDS>("Current Element: ", SystemThemes.MAIN, SystemThemes.INSET);
+		degree_label = new PanelHeader<DriftCorrectionDS>("Degree: ", SystemThemes.MAIN, SystemThemes.INSET);
+	
+		label_one = new PanelHeader<DriftCorrectionDS>(SystemThemes.superscript("Placeholder1"), SystemThemes.MAIN);
+		label_two = new PanelHeader<DriftCorrectionDS>("Placeholder", SystemThemes.MAIN);
+		
+		static_button_width = next_element.getPreferredSize().width;
 		
 		element_selection.addActionListener(new ActionListener () {
 		    public void actionPerformed(ActionEvent e) {
@@ -94,47 +112,57 @@ public class DriftCorrectionSettings<Backend extends DriftCorrectionDS> extends 
 
 	@Override
 	public void on_start() {
-		constraints.gridwidth = 3;
+
+		panel_header.on_start();
+		
+		//constraints.gridy = 0;
+		//enforce side column widths:
+		//constraints.gridwidth = 1;
+		//constraints.gridx = 0;
+		//add(javax.swing.Box.createHorizontalStrut(static_button_width), constraints);
+		//constraints.gridx = 3;
+		//add(javax.swing.Box.createHorizontalStrut(static_button_width), constraints);
+		
+		constraints.gridwidth = 4;
+		constraints.gridy = 1;
+		constraints.gridx = 0;
 		constraints.anchor = GridBagConstraints.NORTH;
 		constraints.ipady = SystemThemes.HEADER_PADDING;
 		
-		PanelHeader<DriftCorrectionDS> panel_header = new PanelHeader<DriftCorrectionDS>("Drift Correction", SystemThemes.MAIN);
-		panel_header.on_start();
+		//add panel title
 		add(panel_header, constraints);
 		
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		constraints.gridwidth = 2;
-		constraints.weightx = 1;
-		constraints.weighty = 0;
-		
-		PanelHeader<DriftCorrectionDS> element_label = new PanelHeader<DriftCorrectionDS>("Current Element: ", SystemThemes.MAIN, SystemThemes.INSET);
 		element_label.on_start();
 		element_selection.setSelectedIndex(current_elem_index);
+		
+		//add element selection dropdown to label
 		element_label.add(element_selection);
 		
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		constraints.gridwidth = 2;
+		constraints.weighty = 0;
+		constraints.weightx = 0.5;
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		
+		//add element panel to main panel
 		add(element_label, constraints);
 		
 		for (int i = 0; i < max_degree; i++) {
 			degree_selection.addItem((Integer) i);
 		}
-		degree_selection.setSelectedIndex(6);
+		degree_selection.setSelectedIndex(default_degree);
+			
 		
-		constraints.gridx = 2;
-		constraints.weightx = 0;
-		constraints.gridy = 1;
-		constraints.gridwidth = 1;
-		
-		constraints.weighty = 0;
-		PanelHeader<DriftCorrectionDS> degree_label = new PanelHeader<DriftCorrectionDS>("Degree: ", SystemThemes.MAIN, SystemThemes.INSET);
 		degree_label.on_start();
 		degree_label.add(degree_selection);
 		
-		add(degree_label, constraints);
+		constraints.gridx = 2;
+		constraints.weightx = 0.5;
+		constraints.gridwidth = 2;
 		
-		constraints.gridy = 2;
-		constraints.gridx = 0;
-		constraints.weightx = 0.25;
+		//add degree label
+		add(degree_label, constraints);
 		
 		prev_element.addActionListener(new ActionListener () {
 		    public void actionPerformed(ActionEvent e) {
@@ -142,16 +170,30 @@ public class DriftCorrectionSettings<Backend extends DriftCorrectionDS> extends 
 		    }
 		});
 		
+		constraints.gridx = 0;
+		constraints.gridwidth = 4;
+		constraints.gridy++;
+		constraints.weighty = 0.5;
+		
+		//add spanning label
+		add(label_one, constraints);
+		
+		//add second spanning label
+		constraints.gridy++;
+		add(label_two, constraints);
+		
+		constraints.gridy++;
+		constraints.gridx = 0;
+		constraints.gridwidth = 1;
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		
 		add(prev_element, constraints);
 		
-		
-		constraints.weightx = 0.5;
+		constraints.weightx = 1;
 		constraints.gridx = 1;
-		
+		constraints.gridwidth = 2;
 		add(calc_fit, constraints);
-		
-		constraints.gridx = 2;
-		constraints.weightx = 0.25;
 		
 		next_element.addActionListener(new ActionListener () {
 		    public void actionPerformed(ActionEvent e) {
@@ -159,6 +201,9 @@ public class DriftCorrectionSettings<Backend extends DriftCorrectionDS> extends 
 		    }
 		});
 		
+		constraints.gridx = 3;
+		constraints.weightx = 0;
+		constraints.gridwidth = 1;
 		add(next_element, constraints);
 		
 		setVisible(true);
--- src/ui_stdlib/SystemThemes.java
+++ src/ui_stdlib/SystemThemes.java
@@ -104,4 +104,18 @@ public class SystemThemes {
 	public static boolean valid_csv(String file_path) {
 		return file_path.endsWith(".csv");
 	}
+	
+	public static String superscript(String str) {
+	    str = str.replaceAll("0", "⁰");
+	    str = str.replaceAll("1", "¹");
+	    str = str.replaceAll("2", "²");
+	    str = str.replaceAll("3", "³");
+	    str = str.replaceAll("4", "⁴");
+	    str = str.replaceAll("5", "⁵");
+	    str = str.replaceAll("6", "⁶");
+	    str = str.replaceAll("7", "⁷");
+	    str = str.replaceAll("8", "⁸");
+	    str = str.replaceAll("9", "⁹");         
+	    return str;
+	}
 }
