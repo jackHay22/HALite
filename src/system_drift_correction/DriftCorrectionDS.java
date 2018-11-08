0b37c352dc10358add5710daddc760cf64d5df77
benprofitt <bparfitt@hamilton.edu>
2018-11-07 20:29:59 -0500

    Filled in get methods for equation and r^2 strings in DCDS

--- src/system_drift_correction/DriftCorrectionDS.java
+++ src/system_drift_correction/DriftCorrectionDS.java
@@ -204,7 +204,6 @@ public class DriftCorrectionDS extends DataBackend implements Refreshable<DriftC
 	
 	public ElementDriftInfo get_plot_info() {
 		ElementDriftInfo info = this.cps_info.get(this.get_element()).get_drift_info();
-		
 		return info;
 	}
 	
@@ -218,11 +217,11 @@ public class DriftCorrectionDS extends DataBackend implements Refreshable<DriftC
 	
 	public String get_eqn() {
 		//TODO: use system themes for superscript
-		return "";
+		return this.get_plot_info().get_equation().get_str_rep();
 	}
 	
 	public String get_rsqrd() {
-		return "";
+		return Double.toString(this.get_plot_info().get_equation().get_r2());
 	}
 
 	@Override
